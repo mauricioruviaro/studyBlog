9fa13d47b1d58f8cd86920510d47c6ff36227290
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2022-08-27 13:26:15 -0300

    instalado cors e usado bodyparser

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
-// app.use(bodyParser);
+app.use(bodyParser.json());
+app.use(cors());
 app.get('/ping', (_req, res) => res.status(200).json({ message: 'Pong' }));
 
 app.listen(PORT, () => console.log(`rodando na porta ${PORT}`));
