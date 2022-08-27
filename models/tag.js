4a3d5b2115b09885fbe1f5a6af66b4b42ad9f0b3
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2022-08-27 16:30:19 -0300

    adicionado model da tabela tag

---
+++ models/tag.js
@@ -0,0 +1,23 @@
+const Sequelize = require('sequelize');
+const database = require('../db.js');
+
+const Tag = database.define('tag', {
+	id: {
+		type: Sequelize.INTEGER,
+		autoIncrement: true,
+		allowNull: false,
+		primaryKey: true,
+	},
+	tag: {
+		type: Sequelize.STRING(30),
+		allowNull: false,
+	},
+	createdAt: {
+		type: Sequelize.DATETIME,
+		defaultValue: Sequelize.NOW,
+	},
+}, {
+	timestamps: true,
+});
+
+module.exports = Tag;
