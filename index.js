e653766e73ee82dff3cbfc6ebe2693e9d3517223
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2022-08-28 12:59:59 -0300

    importados arquivos de rotas e usados em suas respectivas rotas

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const app = express();
-const PORT = 3000;
+const PostsRouter = require('./routes/Posts.js');
+const UsersRouter = require('./routes/Users.js');
+const TagsRouter = require('./routes/Tags.js');
 
+const PORT = 3000;
+const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
+app.use('/posts', PostsRouter);
+app.use('/users', UsersRouter);
+app.use('/tags', TagsRouter);
+
 app.get('/ping', (_req, res) => res.status(200).json({ message: 'Pong' }));
 
 app.listen(PORT, () => console.log(`rodando na porta ${PORT}`));
