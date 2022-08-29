9c7d5efa18e478c8b5456487f1939b423cb23d50
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2022-08-28 22:59:27 -0300

    feat: adicionada funcao de login ao controlador

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,20 @@
-const { validateEmail } = require('../services/userService.js');
+const { login } = require('../services/userService.js');
 
-const testRoute = async (_req, res) => {
-  validateEmail('email.teste@email.com');
+const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) return res.status(400).json({ message: 'Empty fields'});
 
-  res.status(200).json({ message: '/users' });
+  try {
+    const { token, error } = await login(email, password);
+
+    if (error) return res.status(400).json({ message: error});
+
+    res.status(200).json({ token });
+  } catch (error) {
+    res.status(500).json({ message: 'Internal server error. Please, try again later.' });
+  }
 };
 
 module.exports = {
-  testRoute,
+  userLogin,
 }
