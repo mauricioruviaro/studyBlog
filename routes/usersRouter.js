46bff6023c4121851af1ad9c5857fc38d3f1d0a1
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2022-08-28 23:02:32 -0300

    fix: adicionada rota correta para o login, importada funcao de login

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,8 @@
 const { Router } = require('express');
 const AuthToken = require('../middlewares/AuthToken.js');
-const { testRoute } = require('../controllers/userController.js');
+const { userLogin } = require('../controllers/userController.js');
 const Users = Router();
 
-Users.get('/', AuthToken, testRoute);
+Users.get('/signin', userLogin);
 
 module.exports = Users;
