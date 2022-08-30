cfe89c58a620f7a72cb013e31de162d5ad5d3089
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2022-08-29 22:11:58 -0300

    feat: tratados dados e respostas para o cadastro do usuario

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,15 +1,31 @@
-const { login } = require('../services/userService.js');
+const { signIn, signUp } = require('../services/userService.js');
 
-const userLogin = async (req, res) => {
+const userSignIn = async (req, res) => {
   const { email, password } = req.body;
   if (!email || !password) return res.status(400).json({ message: 'Empty fields'});
 
   try {
-    const { token, error } = await login(email, password);
+    const { token, error, status } = await signIn(email, password);
 
-    if (error) return res.status(404).json({ message: error});
+    if (error) return res.status(status).json({ message: error});
 
-    res.status(200).json({ token });
+    res.status(status).json({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Internal server error. Please, try again later.' });
+  }
+};
+
+const userSignUp = async (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) return res.status(400).json({ error: 'Empty fields' });
+
+  try {
+    const { token, error, status } = await signUp(username, email, password);
+
+    if (error) return res.status(status).json({ error });
+
+    res.status(status).json({ token });
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: 'Internal server error. Please, try again later.' });
@@ -17,5 +33,6 @@ const userLogin = async (req, res) => {
 };
 
 module.exports = {
-  userLogin,
+  userSignIn,
+  userSignUp,
 }
