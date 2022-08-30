d81f3e2cc5dd768cb963065ad1013514ca0d86f4
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2022-08-29 22:11:30 -0300

    feat: adicionada funcao para validar o cadastro de usuarios

--- services/userService.js
+++ services/userService.js
@@ -12,34 +12,58 @@ const JWTCONFIG = {
   algorithm: process.env.JWT_ALGORITHM,
 };
 
-const validateEmail = (email) => email.match(REGEXEMAIL);
-
-const validateUsername = (username) => username.match(REGEXUSERNAME);
-
-const userExistsInDatabase = async (loginUsername) => await userModel.findOne({ where: { username: loginUsername } });
-
-const emailExistsInDatabase = async (loginEmail) => await userModel.findOne({ where: { email: loginEmail } });
-
-const makeToken = (id) => jwt.sign({ userId: id }, SECRET, JWTCONFIG);
-
-const makePasswordHash = async (password) => bcrypt.hash(password, ROUNDS);
-
-const validatePassword = async (password, databasePassword) => await bcrypt.compare(password, databasePassword);
-
-const login = async (email, password) => {
-  if (!validateEmail(email)) return { error: 'Invalid fields' };
+const signIn = async (email, password) => {
+  if (!validateEmail(email)) return { error: 'Invalid fields', status: 400 };
 
   const user = await emailExistsInDatabase(email);
 
-  if (!user) return { error: 'Invalid fields' };
+  if (!user) return { error: 'User not found', status: 404 };
 
-  if (!await validatePassword(password, user.password)) return { error: 'Invalid fields' };
+  if (!await validatePassword(password, user.password)) return { error: 'Invalid fields', status: 401 };
 
   const token = makeToken(user.id);
 
-  return { token, error: null };
+  return { token, error: null, status: 200 };
 }
 
+const validateEmail = (email) => email.match(REGEXEMAIL);
+
+const emailExistsInDatabase = async (loginEmail) => await userModel.findOne({ where: { email: loginEmail } });
+
+const validatePassword = async (password, databasePassword) => await bcrypt.compare(password, databasePassword);
+
+const makeToken = (id) => jwt.sign({ userId: id }, SECRET, JWTCONFIG);
+
+const signUp = async (username, email, password) => {
+  if (!validateEmail(email)) return { error: 'Invalid e-mail', status: 400 }
+  if (!checkLengthPassword(password)) return { error: 'Password must be have more than 7 characters', status: 401 }
+  if (!validateUsername(username)) return { error: 'Invalid username', status: 401 }
+
+  if (await emailExistsInDatabase(email)) return { error: 'E-mail already exists', status: 401 }
+  if (await usernameExistsInDatabase(username)) return { error: 'Username already exists', status: 401 }
+
+  const hashedPassword = await makePasswordHash(password);
+
+  const newUser = await userModel.create({
+    username,
+    password: hashedPassword,
+    email,
+  });
+
+  const token = makeToken(newUser.id);
+
+  return { token, error: null, status: 201 }
+}
+
+const checkLengthPassword = (password) => password.length > 7;
+
+const validateUsername = (username) => username.match(REGEXUSERNAME);
+
+const usernameExistsInDatabase = async (loginUsername) => await userModel.findOne({ where: { username: loginUsername } });
+
+const makePasswordHash = async (password) => bcrypt.hash(password, ROUNDS);
+
 module.exports = {
-  login,
+  signIn,
+  signUp,
 }
