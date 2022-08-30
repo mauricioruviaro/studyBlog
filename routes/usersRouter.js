718e0630e7ef57c7f9e19eb9c4c178592262f98f
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2022-08-29 22:12:17 -0300

    feat: importado controlador para o cadastro de usuarios

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const AuthToken = require('../middlewares/AuthToken.js');
-const { userLogin } = require('../controllers/userController.js');
+const { userSignIn, userSignUp } = require('../controllers/userController.js');
 const Users = Router();
 
-Users.get('/signin', userLogin);
+Users.get('/signin', userSignIn);
+Users.post('/signup', userSignUp);
 
 module.exports = Users;
