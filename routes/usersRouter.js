94e5a366e5ae688851f7a9034f8c5513c53a73f8
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2022-08-30 23:09:15 -0300

    feat: adicionada rota para solicitar a alteracao da senha

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const AuthToken = require('../middlewares/AuthToken.js');
-const { userSignIn, userSignUp } = require('../controllers/userController.js');
+const { userSignIn, userSignUp, userForgotPassword } = require('../controllers/userController.js');
 const Users = Router();
 
 Users.get('/signin', userSignIn);
 Users.post('/signup', userSignUp);
+Users.post('/forgotpassword', userForgotPassword);
 
 module.exports = Users;
